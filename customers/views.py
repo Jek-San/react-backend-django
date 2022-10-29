f4591c6db6a907bf90c42f9eb7270355d800e101
Jek-San <ihsanzack@gmail.com>
2022-10-29 09:35:09 +0700

    Test commit di pc

--- customers/views.py
+++ customers/views.py
@@ -1,12 +1,11 @@
 from customers.models import Customer
 from customers.serializers import CustomersSerializer
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def customers(request):
    if request.method == 'GET':
       data = Customer.objects.all()  #invoke serializer and return to client
@@ -21,7 +20,7 @@ def customers(request):
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'POST', 'DELETE'])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def customer(request, id):
    try:
       data = Customer.objects.get(pk=id)  #invoke serializer and return to client
